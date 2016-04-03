48cd94de21903418e337ae475d513e8e2012aefe
knayan2005 <k_nayan2005@yahoo.com>
2016-04-03 14:48:45 +0530

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bookhubApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bookhubApp
+ */
+angular.module('bookhubApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bookhubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bookhubApp
+ */
+angular.module('bookhubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
