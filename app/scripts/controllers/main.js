8580cd5a4b7338b996308081cc172ad92b0a0380
Ashish-Sethi <ashishjain8nov@gmail.com>
2016-04-03 18:06:37 +0530

    first draft

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,15 @@
  * Controller of the bookhubApp
  */
 angular.module('bookhubApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+   	var url = 'https://capillary.0x10.info/api/books?type=json&query=list_books';
+	$http.get(url).success(function(response){
+		$scope.list = response.books;
+	});
   });
